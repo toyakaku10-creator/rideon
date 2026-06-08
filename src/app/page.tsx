3a4144c9b81e4aa36fbfb8b79bb80bfa09bc3ebe
toyakaku10-creator <toyakaku10@gmail.com>
2026-06-08 19:19:26 +0900

    fix: return to route view on demo stop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -832,6 +832,7 @@ export default function Home() {
     isDemoModeRef.current = false;
     setIsDemoMode(false);
     setCurrentSpeed(0);
+    setTab('distance');
   };
 
   const pauseDemo = () => {
